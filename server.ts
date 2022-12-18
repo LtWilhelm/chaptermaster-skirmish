616e118d1c00e28a76751e64e0b28f590d9758d4
LtWilhelm <ironicallyshort737@hotmail.com>
2022-12-17 19:35:50 -0700

    updated to latest sockpuppet

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Sockpuppet } from "https://deno.land/x/sockpuppet@Alpha0.5.3/mod.ts";
+import { Sockpuppet } from "https://deno.land/x/sockpuppet@Alpha0.5.4/mod.ts";
 import { Bson, MongoClient } from 'https://deno.land/x/mongo@v0.31.0/mod.ts';
 
 const client = new MongoClient();
@@ -11,12 +11,12 @@ puppet.use('.*', (packet) => {
   packet.message = JSON.parse(packet.message);
 });
 
-puppet.use('skirmish_', await (packet) => {
-  const gameId = packet.to.split('_')[1];
-  const db = client.database('ChapterMaster');
+// puppet.use('skirmish_', await (packet) => {
+//   const gameId = packet.to.split('_')[1];
+//   const db = client.database('ChapterMaster');
 
-  const games = db.collection('games');
-  games.findAndModify({_id: new Bson.ObjectId(gameId)}, packet.)
-});
+//   const games = db.collection('games');
+//   games.findAndModify({_id: new Bson.ObjectId(gameId)}, packet.)
+// });
 
 puppet.createChannel('chat');
